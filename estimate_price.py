6be957721a468aa6da890f93d7d3ff112661c57a
Hiroaki-K4 <49no19@gmail.com>
2023-07-03 17:01:28 +0900

    Add file of price estimation

--- estimate_price.py
+++ estimate_price.py
@@ -1,10 +1,25 @@
 import argparse
+import yaml
 
+
+def estimate_price(mileage: int, theta_0: float, theta_1: float):
+    print(type(mileage))
+    print(type(theta_0))
+    print(type(theta_1))
+    estimated_price = float(theta_0) + (float(theta_1) * int(mileage))
+
+    return estimated_price
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--mileage')
+    parser.add_argument('--param_yaml')
     args = parser.parse_args()
-    print(args.mileage)
-    # main(args.mileage)
+    with open(args.param_yaml) as param_data:
+        params = yaml.safe_load(param_data)
+        theta_0 = params["param"]["theta0"]
+        theta_1 = params["param"]["theta1"]
+
+    estimated_price = estimate_price(args.mileage, theta_0, theta_1)
+    print("estimated_price: ", estimated_price)
