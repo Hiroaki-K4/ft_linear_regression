42df14664d9bbdadf5457a05c5902ff1035c517c
Hiroaki-K4 <49no19@gmail.com>
2023-07-04 08:47:51 +0900

    Add normalization

--- train.py
+++ train.py
@@ -28,33 +28,64 @@ def calculate_grad_1(mileage_list, price_list, pred_theta_0, pred_theta_1):
     return (-2) * diff_sum / len(mileage_list)
 
 
+def normalize_range(data_list):
+    norm_list = []
+    min_data = min(data_list)
+    max_data = max(data_list)
+    for i in range(len(data_list)):
+        norm_data = (data_list[i] - min_data) / (max_data - min_data)
+        norm_list.append(norm_data)
+
+    return norm_list
+
+
+def reverse_before_normalization(target_value, data_list):
+    min_data = min(data_list)
+    max_data = max(data_list)
+    ori_value = target_value * (max_data - min_data) + min_data
+
+    return ori_value
+
+
 def train(csv_file_path: str, output_param_path: str):
     print(csv_file_path)
     print(output_param_path)
     init_theta_0 = 0
     init_theta_1 = 0
-    learning_rate = 0.000001
-    threshold = 0.0001
+    learning_rate = 0.001
+    threshold = 0.000001
 
     mileage_list, price_list = visualize_data.read_data(csv_file_path)
     print(mileage_list, price_list)
+    # TODO: Visualize normalized data
+    norm_mileage_list = normalize_range(mileage_list)
+    print("norm_mileage_list: ", norm_mileage_list)
+    norm_price_list = normalize_range(price_list)
+    print("norm_price_list: ", norm_price_list)
     pred_theta_0 = init_theta_0
     pred_theta_1 = init_theta_1
-    old_cost = calculate_cost_func(mileage_list, price_list, pred_theta_0, pred_theta_1)
-    iterations = 10000
+    old_cost = calculate_cost_func(norm_mileage_list, norm_price_list, pred_theta_0, pred_theta_1)
+    iterations = 100000
     for i in range(iterations):
-        grad_0 = calculate_grad_0(mileage_list, price_list, pred_theta_0, pred_theta_1)
-        grad_1 = calculate_grad_1(mileage_list, price_list, pred_theta_0, pred_theta_1)
+        grad_0 = calculate_grad_0(norm_mileage_list, norm_price_list, pred_theta_0, pred_theta_1)
+        grad_1 = calculate_grad_1(norm_mileage_list, norm_price_list, pred_theta_0, pred_theta_1)
         print("grad_0: ", grad_0)
         print("grad_1: ", grad_1)
         pred_theta_0 = pred_theta_0 - learning_rate * grad_0
         pred_theta_1 = pred_theta_1 - learning_rate * grad_1
-        cost = calculate_cost_func(mileage_list, price_list, pred_theta_0, pred_theta_1)
+        cost = calculate_cost_func(norm_mileage_list, norm_price_list, pred_theta_0, pred_theta_1)
         if abs(old_cost - cost) < threshold:
+        # if cost < threshold:
+            print("cost: ", cost)
             print("Diff is too small.")
             break
         print("cost: ", cost)
         old_cost = cost
+
+    print("i: ", i)
+    print("pred_theta_0: ", pred_theta_0)
+    print("pred_theta_1: ", pred_theta_1)
+    reverse_before_normalization(pred_theta_0, )
 
 
 if __name__ == '__main__':
