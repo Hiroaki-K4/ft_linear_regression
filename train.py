66ea99fc83e20ad53e2fbf8608495b24bed0bff0
Hiroaki-K4 <49no19@gmail.com>
2023-07-03 21:40:40 +0900

    Update training script

--- train.py
+++ train.py
@@ -1,9 +1,60 @@
 import argparse
+import visualize_data
+
+
+def calculate_cost_func(mileage_list, price_list, pred_theta_0, pred_theta_1):
+    diff_sum = 0
+    for i in range(len(mileage_list)):
+        diff_sum += (price_list[i] - (pred_theta_1 * mileage_list[i] + pred_theta_0)) ** 2
+
+    return diff_sum / len(mileage_list)
+
+
+def calculate_grad_0(mileage_list, price_list, pred_theta_0, pred_theta_1):
+    diff_sum = 0
+    for i in range(len(mileage_list)):
+        diff_sum += price_list[i] - (pred_theta_1 * mileage_list[i] + pred_theta_0)
+
+    print("i: ", i)
+    print("len: ", (-2) * diff_sum / len(mileage_list))
+    return (-2) * diff_sum / len(mileage_list)
+
+
+def calculate_grad_1(mileage_list, price_list, pred_theta_0, pred_theta_1):
+    diff_sum = 0
+    for i in range(len(mileage_list)):
+        diff_sum += mileage_list[i] * (price_list[i] - (pred_theta_1 * mileage_list[i] + pred_theta_0))
+
+    return (-2) * diff_sum / len(mileage_list)
 
 
 def train(csv_file_path: str, output_param_path: str):
     print(csv_file_path)
     print(output_param_path)
+    init_theta_0 = 0
+    init_theta_1 = 0
+    learning_rate = 0.000001
+    threshold = 0.0001
+
+    mileage_list, price_list = visualize_data.read_data(csv_file_path)
+    print(mileage_list, price_list)
+    pred_theta_0 = init_theta_0
+    pred_theta_1 = init_theta_1
+    old_cost = calculate_cost_func(mileage_list, price_list, pred_theta_0, pred_theta_1)
+    iterations = 10000
+    for i in range(iterations):
+        grad_0 = calculate_grad_0(mileage_list, price_list, pred_theta_0, pred_theta_1)
+        grad_1 = calculate_grad_1(mileage_list, price_list, pred_theta_0, pred_theta_1)
+        print("grad_0: ", grad_0)
+        print("grad_1: ", grad_1)
+        pred_theta_0 = pred_theta_0 - learning_rate * grad_0
+        pred_theta_1 = pred_theta_1 - learning_rate * grad_1
+        cost = calculate_cost_func(mileage_list, price_list, pred_theta_0, pred_theta_1)
+        if abs(old_cost - cost) < threshold:
+            print("Diff is too small.")
+            break
+        print("cost: ", cost)
+        old_cost = cost
 
 
 if __name__ == '__main__':
--- visualize_data.py
+++ visualize_data.py
@@ -2,17 +2,16 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 
-def visualize_data(csv_file_path: str):
+def read_data(csv_file_path: str):
     df = pd.read_csv(csv_file_path)
     mileage_list = df["km"].values.tolist()
     price_list = df["price"].values.tolist()
-    print(mileage_list)
-    print(price_list)
-    data_list = []
-    for i in range(len(mileage_list)):
-        data_list.append([mileage_list[i], price_list[i]])
 
-    print(data_list)
+    return mileage_list, price_list
+
+
+def visualize_data(csv_file_path: str):
+    mileage_list, price_list = read_data(csv_file_path)
 
     plt.plot(mileage_list, price_list, 'bo')
     plt.axis([0, max(mileage_list)+10000, 0, max(price_list)+1000])
